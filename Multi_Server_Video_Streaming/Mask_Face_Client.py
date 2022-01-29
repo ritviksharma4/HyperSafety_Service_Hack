e5c4798a6aee47bfc649f02e095dcfa27ad298cd
ritviksharma4 <ritviksharma4@gmail.com>
2022-01-29 12:52:50 +0530

    1 Server 1 Client. Cleaned up the code little

--- Multi_Server_Video_Streaming/Mask_Face_Client.py
+++ Multi_Server_Video_Streaming/Mask_Face_Client.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import socket
-import time
 import pickle
 import struct
 
@@ -10,11 +9,13 @@ import struct
     VideoCapture("your_video_path") to read a specific video.
 """
 
+mask_client_socket = None
 
 # Client communicates with mask detection server.
 
 def comms_server_mask(webcam): 
 
+    global mask_client_socket
     print("In Mask Client")
     mask_client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     mask_client_socket.connect(('localhost',7089))
@@ -39,38 +40,9 @@ def comms_server_mask(webcam):
             else :
                 print("Server Reply :", mask_server_reply)
 
-            if (mask_server_reply == "0"):
-                mask_client_socket.close()
-                comms_server_face_rec(webcam)
         else :
             print("Web-Cam could not be opened")
-        
 
-            
-# Client communicates with the face recog server if mask is not detected.
-
-def comms_server_face_rec(webcam):
-
-    print("Client is in face rec")
-    face_rec_client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    face_rec_client_socket.connect(('localhost',7090))
-
-    while True:
-        ret,frame = webcam.read()
-        # start = time.time()
-        # while (time.time() - start < 1):
-        if (ret == True):
-            data = pickle.dumps(frame)
-            message_size = struct.pack("L", len(data)) 
-            face_rec_client_socket.sendall(message_size + data)
-
-            face_rec_server_reply = face_rec_client_socket.recv(1024)
-            face_rec_server_reply = face_rec_server_reply.decode()
-            print("Face Rec Server Sent :", face_rec_server_reply)
-        
-        face_rec_client_socket.close()
-    
-        comms_server_mask(webcam)
 
 if __name__ == '__main__':
 
@@ -78,7 +50,11 @@ if __name__ == '__main__':
         VideoCapture(0) turns on the webcam.
         VideoCapture("your_video_path") to read a specific video.
     """
-    
-    webcam = cv2.VideoCapture(0)
+    try : 
 
-    comms_server_mask(webcam)
+        webcam = cv2.VideoCapture(0)
+        comms_server_mask(webcam)
+
+    except KeyboardInterrupt:
+        print("\nClosing Client...\n")
+        mask_client_socket.close()
---
+++ Multi_Server_Video_Streaming/Mask_Face_Server.py
@@ -0,0 +1,132 @@
+import pickle
+import socket
+import struct
+from collections import Counter
+from covid_mask_detector.frame_face_rec import detectFace_Mask
+from Face_Recognition_Project.face_recognition.Face_Recognition_By_Frames import face_recognition_service
+
+server_socket = None
+Frame_Mask_Detect_Pair = []
+Output_List = []
+client_socket = None
+
+def create_Output_List():
+
+    global Frame_Mask_Detect_Pair, Output_List
+    for pair in Frame_Mask_Detect_Pair:
+        Output_List.append(pair[0])
+
+def most_probable_mask_detection_face_recognition():
+    print("Finding most Probable..")
+    create_Output_List()
+    global Output_List
+    data = Counter(Output_List)
+    most_frequent = max(Output_List, key=data.get)
+    Output_List = []
+    return most_frequent
+
+def create_server_socket():
+    global server_socket
+    HOST = ''
+    PORT = 7089
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(10)
+
+def send_result_to_client(msg_to_client):
+    
+    encoded_msg_to_client = msg_to_client.encode()
+    client_socket.send(encoded_msg_to_client)
+
+# Server communicates with client and accepts video frames
+def comms_client():
+
+    global Frame_Mask_Detect_Pair, server_socket, client_socket
+
+    client_socket, addr = server_socket.accept()
+
+    data = b''  
+    payload_size = struct.calcsize("L")  
+    
+    print("Got Connection from :", addr)
+
+    while True:
+        # Retrieve message size
+        while len(data) < payload_size:
+            data += client_socket.recv(4096)
+
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("L", packed_msg_size)[0]  
+        
+        # Retrieve all data based on message size
+        while len(data) < msg_size:
+            data += client_socket.recv(4096)
+
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        
+        # Extract frame
+        frame = pickle.loads(frame_data)
+
+        # Pass the frame to the mask detection model to
+        # check if the person is wearing a mask or not
+
+        error_flag = 0
+        try :
+            mask_detect = detectFace_Mask(frame)
+        except ValueError:
+            error_flag = 1
+            mask_detect = "*** NO FACE DETECTED! ***"
+
+        print("Mask_Detect :", mask_detect)
+        if (error_flag == 1):
+            mask_detect = "No Face Detected"
+            error_flag = 0
+
+        if (mask_detect == None):
+            mask_detect = "No Face Detected"        
+        
+        Frame_Mask_Detect_Pair.append((mask_detect, frame))
+        
+        print("Mask Prediction :", mask_detect)
+        # print("Output :", Frame_Mask_Detect_Pair)
+
+        print("Output Lists Size:", len(Frame_Mask_Detect_Pair))
+        
+        """
+            For every 25 predictions for the same person, 
+            we pick the most frequent prediction and send
+            the result to the client.
+        """
+        if (len(Frame_Mask_Detect_Pair) == 25 or (mask_detect == "No Face Detected" and len(Frame_Mask_Detect_Pair) > 0)):
+            final_mask_detection = most_probable_mask_detection_face_recognition()
+            print("Found Most Probable :", final_mask_detection)
+
+            if (final_mask_detection == "No Mask"):
+                face_recog_reply = face_recognition_service(Frame_Mask_Detect_Pair)
+                msg_to_client = final_mask_detection + "\nPerson Found : " + face_recog_reply         
+
+            elif (final_mask_detection == "Mask"):
+                msg_to_client = "Wearing a Mask"
+
+
+            elif (final_mask_detection == "No Face Detected"):
+                msg_to_client = "Still Processing..."
+            
+            Frame_Mask_Detect_Pair = []
+
+        else :
+            msg_to_client = "Still Processing..."
+            
+        send_result_to_client(msg_to_client)
+
+
+if __name__ == '__main__':
+    try :
+        create_server_socket()
+        print('Mask Detection - Face Recognition Server is Running...\n')
+        comms_client()
+    except KeyboardInterrupt:
+        server_socket.close()
+        print("\n\nMask Detection - Face Recognition Server has Shutdown.\n")
